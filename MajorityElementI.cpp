2a5f11a1b759f3895757232792a4c8af01f97bca
123456-manvi <103314650+123456-manvi@users.noreply.github.com>
2024-02-20 19:03:49 +0530

    Create MajorityElementI.cpp

---
+++ MajorityElementI.cpp
@@ -0,0 +1,41 @@
+// for N/2 element.........
+#include<iostream>
+using namespace std;
+class Solution{
+public:
+vector<int> MajorityElement(const vector<int> &A){
+  int c=0;
+int el;
+int n=A.size();
+for(int i=0;i<n;i++)
+{
+if(c==0)
+{
+c++;
+el=A[i];
+}
+else if(A[i]==el)
+{
+c++;
+}
+else{
+c--;
+}
+}
+
+// now check for n/2 element..
+int c1=0;
+for(int i=0;i<n;i++)
+{
+if(A[i]==el)
+{
+c1++;
+}
+}
+if(c1>n/2)
+{
+return el;
+}
+return -1;
+}
+};
